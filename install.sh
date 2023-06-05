01b78cb7a85428fe4f967ddd0f782e44d03c1f31
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-06-05 10:13:26 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 script_path=$(pwd)
 tntsms_content="#!/bin/bash\n\ncd $script_path\npython3 main.py"
 
-echo -e $tntsms_content > /usr/local/bin/tntsms.sh
+echo -e $tntsms_content > /usr/local/bin/proxyuni.sh
+chmod +x /usr/local/bin/proxyuni.sh
